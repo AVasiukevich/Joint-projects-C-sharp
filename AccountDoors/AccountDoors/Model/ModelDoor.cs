60188d72579434bfddc58a15988738d62bb06d20
Alexander <dreadnight@tut.by>
2017-09-18 14:47:57 +0300

    Project on WPF
    The program for the salon of doors

---
+++ AccountDoors/AccountDoors/Model/ModelDoor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountDoors.Model
+{
+    public class ModelDoor
+    {
+        private string name;
+        private double price;
+        private string picture;
+
+        public string Picture
+        {
+            get { return picture; }
+            set { picture = value; }
+        }
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public override string ToString()
+        {
+            return String.Format($"{name} {price} {picture}");
+        }
+    }
+}
---
+++ AccountDoors/AccountDoors/ViewModel/ViewModelBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountDoors.ViewModel
+{
+    public class ViewModelBase : INotifyPropertyChanged , IDisposable
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName)); //[CallerMemberName]string propertyName = ""
+            }
+        }
+
+        public void Dispose()
+        {
+            this.OnDispose();
+        }
+
+        protected virtual void OnDispose()
+        {}
+    }
+}
